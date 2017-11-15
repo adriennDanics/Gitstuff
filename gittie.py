d524e6f2693bd6edb3d4004193fb46326892e37d
Adrienn Dánics <adrienn.d91@gmail.com>
2017-11-15 09:57:51 +0100

    Hiiiiiyyyyaaaa

--- gittie.py
+++ gittie.py
@@ -2,13 +2,18 @@ def introduce():
     print("Hello! I'm a gittiest Gitty!")
 
 
-
 def joke():
     print("Jó leggelt!")
+
 
 def add(a, b):
     d = a + b
     return d
 
+
 def shout():
     print("...")
+
+
+def shout2():
+    print("THIS IS GITHUB!!!")
